03df51c23b4e4e6687a4197794e5b530bb4085de
DiKo-bu <kdmitri30@gmail.com>
2026-05-14 22:55:07 +0500

    Update simulation_task.dart

--- lib/logic/simulation_task.dart
+++ lib/logic/simulation_task.dart
@@ -1,59 +1,54 @@
-import 'dart:math';
-
 class SimulationTask {
-  String id;
+  int id;
   String name;
-  double min, likely, max;
-  List<String> dependsOn;
+  double min;
+  double likely;
+  double max;
+  List<int> dependsOn;
   bool isCompleted;
-  double actualDuration;
-  DateTime? actualEndDate;   // новое поле
+  double? actualDuration;
+  DateTime? actualEndDate;
 
-  // поля для Посадки
-  String? plantingType;
-  String? culture;
-  double? plantingQuantity;
-  double? plantingArea;
-
-  // поля для Посева
-  String? sowingBreed;
-  double? sowingQuantityKg;
-  double? sowingAreaHa;
-
-  // поля для Выборочной санитарной рубки
-  double? cuttingArea;
-  double? cuttingVolume;
-
-  // поля для Сплошной санитарной рубки
+  // Поля лесоустройства (используются в UI и для отчетов)
+  String plantingType;      // Тип посадки (Сеянцы, саженцы и т.д.)
+  String culture;           // Культура (Вяз, Ясень, Тополь и т.д.)
+  double? plantingQuantity; // Количество (шт)
+  double? plantingArea;     // Площадь (га)
+  
+  String sowingBreed;       // Порода при посеве
+  double? sowingQuantityKg; // Количество семян (кг)
+  double? sowingAreaHa;     // Площадь посева (га)
+  
+  double? cuttingArea;      // Площадь рубки
+  double? cuttingVolume;    // Объем рубки (м3)
+  
   double? clearCuttingArea;
   double? clearCuttingVolume;
-
-  // поля для Уборки захламленности
-  double? clearingArea;
+  
+  double? clearingArea;     // Очистка
   double? clearingVolume;
-
-  // поле для Установки панно и аншлагов
-  double? panelsQuantity;
-
-  String? location;
-  String? quarter;
-  String? allotment;
+  
+  int? panelsQuantity;      // Количество аншлагов
+  
+  String location;          // Местонахождение
+  int? quarter;             // Квартал
+  int? allotment;           // Выдел
 
   SimulationTask({
     required this.id,
     required this.name,
-    this.min = 0,
-    this.likely = 0,
-    this.max = 0,
+    required this.min,
+    required this.likely,
+    required this.max,
     this.dependsOn = const [],
     this.isCompleted = false,
-    this.actualDuration = 0,
+    this.actualDuration,
     this.actualEndDate,
-    this.plantingType,
-    this.culture,
+    this.plantingType = 'Сеянцы',
+    this.culture = 'Вяз',
     this.plantingQuantity,
     this.plantingArea,
-    this.sowingBreed,
+    this.sowingBreed = '',
     this.sowingQuantityKg,
     this.sowingAreaHa,
     this.cuttingArea,
@@ -63,12 +58,45 @@ class SimulationTask {
     this.clearingArea,
     this.clearingVolume,
     this.panelsQuantity,
-    this.location,
+    this.location = '',
     this.quarter,
     this.allotment,
   });
 
-  Map<String, dynamic> toMap() => {
+  // Фабричный метод для создания объекта из JSON (загрузка из Hive/Сервера)
+  factory SimulationTask.fromJson(Map<String, dynamic> json) {
+    return SimulationTask(
+      id: json['id'] ?? 0,
+      name: json['name'] ?? '',
+      min: (json['min'] as num?)?.toDouble() ?? 1.0,
+      likely: (json['likely'] as num?)?.toDouble() ?? 2.0,
+      max: (json['max'] as num?)?.toDouble() ?? 3.0,
+      dependsOn: json['dependsOn'] != null ? List<int>.from(json['dependsOn']) : [],
+      isCompleted: json['isCompleted'] ?? false,
+      actualDuration: (json['actualDuration'] as num?)?.toDouble(),
+      actualEndDate: json['actualEndDate'] != null ? DateTime.parse(json['actualEndDate']) : null,
+      plantingType: json['plantingType'] ?? 'Сеянцы',
+      culture: json['culture'] ?? 'Вяз',
+      plantingQuantity: (json['plantingQuantity'] as num?)?.toDouble(),
+      plantingArea: (json['plantingArea'] as num?)?.toDouble(),
+      sowingBreed: json['sowingBreed'] ?? '',
+      sowingQuantityKg: (json['sowingQuantityKg'] as num?)?.toDouble(),
+      sowingAreaHa: (json['sowingAreaHa'] as num?)?.toDouble(),
+      cuttingArea: (json['cuttingArea'] as num?)?.toDouble(),
+      cuttingVolume: (json['cuttingVolume'] as num?)?.toDouble(),
+      clearCuttingArea: (json['clearCuttingArea'] as num?)?.toDouble(),
+      clearCuttingVolume: (json['clearCuttingVolume'] as num?)?.toDouble(),
+      clearingArea: (json['clearingArea'] as num?)?.toDouble(),
+      clearingVolume: (json['clearingVolume'] as num?)?.toDouble(),
+      panelsQuantity: json['panelsQuantity'] as int?,
+      location: json['location'] ?? '',
+      quarter: json['quarter'] as int?,
+      allotment: json['allotment'] as int?,
+    );
+  }
+
+  // Метод для преобразования объекта в JSON (сохранение в Hive/Сервер)
+  Map<String, dynamic> toJson() => {
     'id': id,
     'name': name,
     'min': min,
@@ -77,63 +105,23 @@ class SimulationTask {
     'dependsOn': dependsOn,
     'isCompleted': isCompleted,
     'actualDuration': actualDuration,
-    if (actualEndDate != null) 'actualEndDate': actualEndDate!.toIso8601String(),
-    if (plantingType != null) 'plantingType': plantingType,
-    if (culture != null) 'culture': culture,
-    if (plantingQuantity != null) 'plantingQuantity': plantingQuantity,
-    if (plantingArea != null) 'plantingArea': plantingArea,
-    if (sowingBreed != null) 'sowingBreed': sowingBreed,
-    if (sowingQuantityKg != null) 'sowingQuantityKg': sowingQuantityKg,
-    if (sowingAreaHa != null) 'sowingAreaHa': sowingAreaHa,
-    if (cuttingArea != null) 'cuttingArea': cuttingArea,
-    if (cuttingVolume != null) 'cuttingVolume': cuttingVolume,
-    if (clearCuttingArea != null) 'clearCuttingArea': clearCuttingArea,
-    if (clearCuttingVolume != null) 'clearCuttingVolume': clearCuttingVolume,
-    if (clearingArea != null) 'clearingArea': clearingArea,
-    if (clearingVolume != null) 'clearingVolume': clearingVolume,
-    if (panelsQuantity != null) 'panelsQuantity': panelsQuantity,
-    if (location != null) 'location': location,
-    if (quarter != null) 'quarter': quarter,
-    if (allotment != null) 'allotment': allotment,
+    'actualEndDate': actualEndDate?.toIso8601String(),
+    'plantingType': plantingType,
+    'culture': culture,
+    'plantingQuantity': plantingQuantity,
+    'plantingArea': plantingArea,
+    'sowingBreed': sowingBreed,
+    'sowingQuantityKg': sowingQuantityKg,
+    'sowingAreaHa': sowingAreaHa,
+    'cuttingArea': cuttingArea,
+    'cuttingVolume': cuttingVolume,
+    'clearCuttingArea': clearCuttingArea,
+    'clearCuttingVolume': clearCuttingVolume,
+    'clearingArea': clearingArea,
+    'clearingVolume': clearingVolume,
+    'panelsQuantity': panelsQuantity,
+    'location': location,
+    'quarter': quarter,
+    'allotment': allotment,
   };
-
-  factory SimulationTask.fromMap(Map<dynamic, dynamic> map) => SimulationTask(
-    id: map['id'],
-    name: map['name'],
-    min: (map['min'] ?? 0).toDouble(),
-    likely: (map['likely'] ?? 0).toDouble(),
-    max: (map['max'] ?? 0).toDouble(),
-    dependsOn: List<String>.from(map['dependsOn'] ?? []),
-    isCompleted: map['isCompleted'] ?? false,
-    actualDuration: (map['actualDuration'] ?? 0).toDouble(),
-    actualEndDate: map['actualEndDate'] != null ? DateTime.tryParse(map['actualEndDate']) : null,
-    plantingType: map['plantingType'],
-    culture: map['culture'],
-    plantingQuantity: map['plantingQuantity']?.toDouble(),
-    plantingArea: map['plantingArea']?.toDouble(),
-    sowingBreed: map['sowingBreed'],
-    sowingQuantityKg: map['sowingQuantityKg']?.toDouble(),
-    sowingAreaHa: map['sowingAreaHa']?.toDouble(),
-    cuttingArea: map['cuttingArea']?.toDouble(),
-    cuttingVolume: map['cuttingVolume']?.toDouble(),
-    clearCuttingArea: map['clearCuttingArea']?.toDouble(),
-    clearCuttingVolume: map['clearCuttingVolume']?.toDouble(),
-    clearingArea: map['clearingArea']?.toDouble(),
-    clearingVolume: map['clearingVolume']?.toDouble(),
-    panelsQuantity: map['panelsQuantity']?.toDouble(),
-    location: map['location'],
-    quarter: map['quarter'],
-    allotment: map['allotment'],
-  );
-
-  double getSample(Random rnd) {
-    if (isCompleted) return actualDuration;
-    if (max <= min) return min;
-    double r = rnd.nextDouble();
-    if (r < (likely - min) / (max - min)) {
-      return min + sqrt(r * (max - min) * (likely - min));
-    } else {
-      return max - sqrt((1 - r) * (max - min) * (max - likely));
-    }
-  }
 }

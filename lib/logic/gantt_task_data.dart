630b9e4f8d27be7d8a8010f227f26903cfba812a
DiKo-bu <kdmitri30@gmail.com>
2026-05-15 17:10:50 +0500

    Update gantt_task_data.dart

--- lib/logic/gantt_task_data.dart
+++ lib/logic/gantt_task_data.dart
@@ -1,10 +1,13 @@
 class GanttTaskData {
+  final String taskId;
   final String name;
   final double startTime;
   final double endTime;
   final bool isCompleted;
   final bool isOverMax;
+
   GanttTaskData({
+    required this.taskId,
     required this.name,
     required this.startTime,
     required this.endTime,

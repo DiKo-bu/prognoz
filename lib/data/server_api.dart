d269f911380f83ef6e5a7ea0d35fc2cdb79a183d
DiKo-bu <kdmitri30@gmail.com>
2026-05-16 20:41:29 +0500

    Fix server URL usage and report endpoint

--- lib/data/server_api.dart
+++ lib/data/server_api.dart
@@ -1,9 +1,12 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+/// Получение последнего отчёта по исполнителю
 Future<String?> fetchPlan(String serverUrl, String executor) async {
   try {
-    final url = Uri.parse("$serverUrl/plan?executor=$executor");
+    // БЕРЁМ РОВНО ТО, ЧТО ТЫ ВВЁЛ В НАСТРОЙКАХ:
+    // https://rebound-plus-latter-motivation.trycloudflare.com
+    // и добавляем /report/<executor>
+    final url = Uri.parse('$serverUrl/report/$executor');
 
     final response = await http.get(url);
 
